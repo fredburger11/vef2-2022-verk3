3e514e3ade8d796c0704f528da724172d366f2a0
fredburger11 <32545517+fredburger11@users.noreply.github.com>
2022-03-04 12:39:01 +0000

    slóðir tilbúnar

--- src/lib/template-helpers.js
+++ src/lib/template-helpers.js
@@ -5,8 +5,10 @@
  * @param {array} errors Fylki af villum frá express-validator pakkanum
  * @returns {boolean} `true` ef `field` er í `errors`, `false` annars
  */
+/*
 export function isInvalid(field, errors = []) {
   // Boolean skilar `true` ef gildi er truthy (eitthvað fannst)
   // eða `false` ef gildi er falsy (ekkert fannst: null)
   return Boolean(errors.find((i) => i && i.param === field));
 }
+*/
